7279840e8f1daab28b7617f1ac95b61b8b8339e4
Sunil Kumar14 <sunil.kumar14@oyorooms.com>
2022-04-22 14:15:44 +0530

    initial commit

---
+++ Test/Shared/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Shared
+//
+//  Created by Sunil Kumar on 22/04/22.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
